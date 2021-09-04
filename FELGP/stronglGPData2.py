4f0c95434169e17e55d63592949195a36b2a5710
Ying Bi <50221719+YingBi92@users.noreply.github.com>
2021-09-04 19:45:54 +1200

    Add files via upload

---
+++ FELGP/stronglGPData2.py
@@ -0,0 +1,183 @@
+import numpy
+from numpy import ndarray
+
+
+class learningrate:
+    def __init__(float):
+        pass
+
+
+class cpanaety:
+    def __init__(float):
+        pass
+
+
+class numTree:
+    def __init__(int):
+        pass
+
+
+class treeDepth:
+    def __init__(int):
+        pass
+
+
+class kernelSize:
+    def __init__(int):
+        pass
+
+
+class poolingType:
+    def __init__(ndarray):
+        pass
+
+
+class trainLabel:
+    def __init__(ndarray):
+        pass
+
+
+class trainData:
+    def __init__(ndarray):
+        pass
+
+
+class testData:
+    def __init__(ndarray):
+        pass
+
+
+class imageDa:
+    def __init__(ndarray):
+        pass
+
+
+class indexType:
+    def __init__(int):
+        pass
+
+
+class numInts:
+    def __init__(int):
+        pass
+        pass
+
+
+class numClass:
+    def __init__(int):
+        pass
+
+
+class numDepth:
+    def __init__(int):
+        pass
+
+
+class indexType2:
+    def __init__(int):
+        pass
+
+
+class indexType3:
+    def __init__(int):
+        pass
+
+
+class indexType4:
+    def __init__(int):
+        pass
+
+
+class windowSize:
+    def __init__(int):
+        pass
+
+
+class windowSize1:
+    def __init__(int):
+        pass
+
+
+class windowSize2:
+    def __init__(int):
+        pass
+
+
+class windowSize3:
+    def __init__(int):
+        pass
+
+
+class region:
+    def __init__(ndarray):
+        pass
+
+
+class coordsX:
+    def __init__(int):
+        pass
+
+
+class coordsY:
+    def __init__(int):
+        pass
+
+
+class indexType1:
+    def __init__(int):
+        pass
+
+
+class histdata:
+    def __init__(float):
+        pass
+
+
+class filterData:
+    def __init__(ndarray):
+        pass
+
+
+class filterData2:
+    def __init__(ndarray):
+        pass
+
+
+class poolingData2:
+    def __init__(ndarray):
+        pass
+
+
+class filterData3:
+    def __init__(ndarray):
+        pass
+
+
+class poolingData3:
+    def __init__(ndarray):
+        pass
+
+
+class coordsX1:
+    def __init__(int):
+        pass
+
+
+class coordsY1:
+    def __init__(int):
+        pass
+
+
+class coordsX2:
+    def __init__(int):
+        pass
+
+
+class coordsY2:
+    def __init__(int):
+        pass
+
+
+class randomD:
+    def __init__(float):
+        pass
